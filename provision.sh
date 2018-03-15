e63dd045cf70c831a7a11a2c34a997905cf87bee
xuweicheng <weicheng.xu@gmail.com>
2018-03-14 22:29:58 -0700

    add nginx default config to provision process

--- provision.sh
+++ provision.sh
@@ -2,7 +2,10 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /usr/share/nginx/www
-ln -s /vagrant/www /usr/share/nginx/www
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
+# rm -rf /usr/share/nginx/www
+# ln -s /vagrant/www /usr/share/nginx/www
 
 service nginx start
